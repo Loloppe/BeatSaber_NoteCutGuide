dc35af0805a2d488122ddeb9f32110ca0e6b87a3
Loloppe <LaurentBlaisCote@outlook.com>
2024-08-16 23:24:02 -0400

    Fixed internal restart crash. Color doesn't always match attached note and need to be fixed.

--- HarmonyPatches/GuideInitializer.cs
+++ HarmonyPatches/GuideInitializer.cs
@@ -1,34 +1,33 @@
 ﻿using HarmonyLib;
 using UnityEngine;
-using IPA.Utilities;
 using NoteCutGuide.Algorithm;
 
 namespace NoteCutGuide.HarmonyPatches {
 	[HarmonyPatch(typeof(ColorNoteVisuals), nameof(ColorNoteVisuals.HandleNoteControllerDidInit))]
 	static class GuideInitializer {
-		static FieldAccessor<ColorNoteVisuals, Color>.Accessor ColorNoteVisuals_noteColor = FieldAccessor<ColorNoteVisuals, Color>.GetAccessor("_noteColor");
-
-		static void Postfix(ColorNoteVisuals __instance, NoteControllerBase noteController) {
-			var guide = noteController.transform.Find("NoteCube/NoteCutGuide");
+		static void Postfix(ref NoteControllerBase noteController, ref ColorManager ____colorManager) {
+			var guide = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			GameObject.Destroy(guide.GetComponent<BoxCollider>());
+			var renderer = guide.GetComponent<MeshRenderer>();
+			var NoteCube = noteController.transform.Find("NoteCube").GetComponentInChildren<CutoutEffect>();
+			renderer.material = NoteCube.GetComponentInChildren<MeshRenderer>().material;
+			guide.transform.SetParent(NoteCube.transform);
+			var data = noteController.noteData;
 
 			if(guide == null)
 				return;
 
-			// The guide need to be disabled before returning.
-			if(!Config.Instance.Enabled || Plugin.levelData == null) {
-				guide.gameObject.SetActive(false);
+			if(!Config.Instance.Enabled || BS_Utils.Plugin.LevelData == null || BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData == null) {
 				return;
 			}
 
-			if(noteController.noteData.cutDirection == NoteCutDirection.Any) {
-				guide.gameObject.SetActive(false);
+			if(data.cutDirection == NoteCutDirection.Any) {
 				return;
 			}
 
 			// No GN or DA. The plugin is not compatible with Pro Mode/Strict Angle, so removed for performance.
-			if(Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.ghostNotes || Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.disappearingArrows ||
-				Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.proMode || Plugin.levelData.GameplayCoreSceneSetupData.gameplayModifiers.strictAngles) {
-				guide.gameObject.SetActive(false);
+			if(BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData.gameplayModifiers.ghostNotes || BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData.gameplayModifiers.disappearingArrows ||
+				BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData.gameplayModifiers.proMode || BS_Utils.Plugin.LevelData.GameplayCoreSceneSetupData.gameplayModifiers.strictAngles) {
 				return;
 			}
 
@@ -40,21 +39,18 @@ namespace NoteCutGuide.HarmonyPatches {
 			}
 
 			// Reset the position just in case
-			guide.position = guide.parent.position;
-			guide.rotation = guide.parent.rotation;
+			guide.transform.position = guide.transform.parent.position;
+			guide.transform.rotation = guide.transform.parent.rotation;
 
 			// Change scale according to config
-			guide.localScale = new Vector3(Config.Instance.Width, Config.Instance.Height, Config.Instance.Depth);
+			guide.transform.localScale = new Vector3(Config.Instance.Width, Config.Instance.Height, Config.Instance.Depth);
 
 			// Add an offset to the position
 			guide.transform.localPosition = new Vector3(Config.Instance.X, Config.Instance.Y, Config.Instance.Z);
 
 			// Bloom
-			var renderer = guide.GetComponent<MeshRenderer>();
 			if(Config.Instance.Bloom) {
 				renderer.material.shader = Shader.Find("UI/Default");
-			} else {
-				renderer.material.shader = Plugin.DefaultShader;
 			}
 			
 			if(Config.Instance.Rainbow) { // Random colors
@@ -65,31 +61,28 @@ namespace NoteCutGuide.HarmonyPatches {
 				}
 			} else if(Config.Instance.Color) { // Custom colors
 				if(Config.Instance.Bloom) {
-					if(noteController.noteData.colorType == ColorType.ColorA) { 
+					if(data.colorType == ColorType.ColorA) { 
 						renderer.material.color = ColorWithAlpha(Config.Instance.Left, Config.Instance.Brightness);
-					} else if(noteController.noteData.colorType == ColorType.ColorB) {
+					} else if(data.colorType == ColorType.ColorB) {
 						renderer.material.color = ColorWithAlpha(Config.Instance.Right, Config.Instance.Brightness);
 					}
 				} else {
-					if(noteController.noteData.colorType == ColorType.ColorA) { 
+					if(data.colorType == ColorType.ColorA) { 
 						renderer.material.color = ColorWithAlpha(Config.Instance.Left, 1f);
-					} else if(noteController.noteData.colorType == ColorType.ColorB) {
+					} else if(data.colorType == ColorType.ColorB) {
 						renderer.material.color = ColorWithAlpha(Config.Instance.Right, 1f);
 					}
 				}
 			} else { // Default colors
 				if(Config.Instance.Bloom) {
-					renderer.material.color = ColorWithAlpha(ColorNoteVisuals_noteColor(ref __instance), Config.Instance.Brightness);
+					renderer.material.color = ColorWithAlpha(____colorManager.ColorForType(data.colorType), Config.Instance.Brightness);
 				} else {
-					renderer.material.color = ColorWithAlpha(ColorNoteVisuals_noteColor(ref __instance), 1f);
+					renderer.material.color = ColorWithAlpha(____colorManager.ColorForType(data.colorType), 1f);
 				}
 			}
-
-			// Activate/Disable
-			guide.gameObject.SetActive(true);
 		}
 
-		public static Color ColorWithAlpha(this Color color, float alpha) {
+		public static Color ColorWithAlpha(Color color, float alpha) {
 			color.a = alpha;
 			return color;
 		}
--- Plugin.cs
+++ Plugin.cs
@@ -4,8 +4,6 @@ using HarmonyLib;
 using IPA;
 using IPA.Config.Stores;
 using System.Reflection;
-using UnityEngine;
-using UnityEngine.SceneManagement;
 using IPALogger = IPA.Logging.Logger;
 
 namespace NoteCutGuide {
@@ -15,12 +13,8 @@ namespace NoteCutGuide {
 		internal static IPALogger Log;
 		internal static Harmony harmony;
 
-		// Shader
-		internal static Shader DefaultShader;
 		// Rainbow
 		internal static float RainbowPls = 0f;
-		// Modifier
-		internal static BS_Utils.Gameplay.LevelData levelData = null;
 
 		[Init]
 		public Plugin(IPALogger logger, IPA.Config.Config conf) {
@@ -32,21 +26,13 @@ namespace NoteCutGuide {
 
 		[OnEnable]
 		public void OnEnable() {
-			SceneManager.activeSceneChanged += OnActiveSceneChanged;
 			harmony.PatchAll(Assembly.GetExecutingAssembly());
 			BSMLSettings.instance.AddSettingsMenu("NoteCutGuide", "NoteCutGuide.Views.settings.bsml", Config.Instance);
 			GameplaySetup.instance.AddTab("NoteCutGuide", "NoteCutGuide.Views.settings.bsml", Config.Instance, MenuType.All);
 		}
 
-		public void OnActiveSceneChanged(Scene arg0, Scene scene) {
-			if(BS_Utils.SceneNames.Game == scene.name) {
-				levelData = BS_Utils.Plugin.LevelData;
-			}
-		}
-
 		[OnDisable]
 		public void OnDisable() {
-			SceneManager.activeSceneChanged -= OnActiveSceneChanged;
 			harmony.UnpatchSelf();
 			BSMLSettings.instance.RemoveSettingsMenu(Config.Instance);
 			GameplaySetup.instance.RemoveTab("NoteCutGuide");
